616ad8adba385ef0070d6aac69c008b1436eb43d
farmanp <farmanp.dev@gmail.com>
2021-02-08 22:49:37 -0700

    Initial implementation

--- spec/word_wrapper_spec.rb
+++ spec/word_wrapper_spec.rb
@@ -1,12 +1,20 @@
 require 'word_wrapper' 
+require 'io/console'
 
 describe WordWrapper do 
   describe "#wrap" do
     it "wraps around lines based on column size" do 
-      text = "jfas jf alkdkljf "
-      expected = " "
-      wrapped = WordWrapper.new(text, 10)
-      expect(wrapped).to eq expected
+      text_blob = "The quick fox jumped over the sleeping dog."
+      expected =  "The\nquick\nfox\njumpe\nd\nover\nthe\nsleep\ning\ndog.\n"
+      wrapped = WordWrapper.new(text_blob, 5)
+      expect(wrapped.wrap(text_blob, 5)).to eq expected
     end 
+
+    it "wraps around a bigger file" do 
+      text_blob = IO.readlines("./test/gettysburg_address.txt")
+      expected = "idk"
+      wrapped = WordWrapper.new(text_blob, 100)
+      expect(wrapped.wrap(text_blob, 100)).to eq expected
+    end      
   end 
 end 
